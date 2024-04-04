d218e8ba35ea6bcd8586b128fe080d51b29cc738
Paska2005 <114850600+Pashka385@users.noreply.github.com>
2024-04-04 13:58:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const GetRoad = document.querySelectorAll(".get");
 const StrText = document.querySelectorAll(".answer__li");
+const NameCom = document.querySelector(".name_text");
+const box = document.querySelector(".name");
+const NumberGroup = document.querySelector(".group");
+
+function getName(message) {
+    return prompt(message);
+}
+function getNumberGroup(message) {
+    return prompt(message);
+}
+
+let resultName = getName('Введите название команды');
+NameCom.textContent = resultName;
+
+let resultGroup = getNumberGroup('Введите номер группы');
+NumberGroup.textContent = resultGroup;
+
+box.addEventListener('click', () => {
+    let resultName = getName('Введите название команды');
+    NameCom.textContent = resultName;
+});
+
+NumberGroup.addEventListener('click', () => {
+    let resultGroup = getNumberGroup('Введите номер группы');
+    NumberGroup.textContent = resultGroup;
+});
+
 GetRoad.forEach((event) => {
     event.addEventListener('click', () => {
         if (event.checked) {
